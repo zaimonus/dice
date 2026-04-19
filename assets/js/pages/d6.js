7665d5c2cd89150b2c821b16935c9aae49b3076b
Simon Große Beckmann <simon.groseb@gmail.com>
2026-04-19 19:47:36 +0200

    Refactor die rotation logic for better randomness
    - Get random amount of rotations and multiply it to get degrees, instead of getting a random amount of degrees and trimming that.
    - Define variables before loop to make them available afterwards.

--- assets/js/pages/d6.js
+++ assets/js/pages/d6.js
@@ -1,11 +1,16 @@
 (() => {
     const die = document.querySelector('.die');
+    const SPIN_RANGE = 20;
+    const FULL_ROTATION = 360;
     let previous_rotation_x = 0;
     let previous_rotation_y = 0;
 
     die.addEventListener("click", (event) => {
         const number = Math.floor(Math.random() * 6) + 1;
+        
         let end_position_x, end_position_y;
+        let final_rotation_x, final_rotation_y;
+        
         switch (number) {
             case 1:
                 end_position_x = 0;
@@ -36,29 +41,25 @@
         }
         
         do {
-            const extra_rotation_x = (Math.random() * 3600);
-            const rotation_overflow_x = extra_rotation_x % 360;
-            const trimmed_rotations_x = extra_rotation_x - rotation_overflow_x;
-            const final_rotation_x = end_position_x + trimmed_rotations_x;
+            // get some extra rotations
+            const extra_rotation_x = Math.floor(Math.random() * SPIN_RANGE);
+            const extra_degrees_x = extra_rotation_x * FULL_ROTATION;
+            final_rotation_x = end_position_x + extra_degrees_x;
+            // ensure final rotation is different than the previous rotation
         } while (final_rotation_x == previous_rotation_x);
         
         do {
-            const extra_rotation_y = (Math.random() * 3600);
-            const rotation_overflow_y = extra_rotation_y % 360;
-            const trimmed_rotations_y = extra_rotation_y - rotation_overflow_y;
-            const final_rotation_y = end_position_y + trimmed_rotations_y;
+            // get some extra rotations
+            const extra_rotation_y = Math.floor(Math.random() * SPIN_RANGE);
+            const extra_degrees_y = extra_rotation_y * FULL_ROTATION;
+            final_rotation_y = end_position_y + extra_degrees_y;
+            // ensure final rotation is different than the previous rotation
         } while (final_rotation_y == previous_rotation_y);
 
+        die.style.transform = `rotateX(${final_rotation_x}deg) rotateY(${final_rotation_y}deg)`;
+
+        // set previous rotation for the next click
         previous_rotation_x = final_rotation_x;
         previous_rotation_y = final_rotation_y;
-    
-        // console.log(`number: ${number}`);
-        // console.log(`end: ${end_position_x}, ${end_position_y}`);
-        // console.log(`extra: ${extra_rotation_x}, ${extra_rotation_y}`);
-        // console.log(`overflow: ${rotation_overflow_x}, ${rotation_overflow_y}`);
-        // console.log(`trimmed: ${trimmed_rotations_x}, ${trimmed_rotations_y}`);
-        // console.log(`final: ${final_rotation_x}, ${final_rotation_y}`);
-
-        die.style.transform = `rotateX(${final_rotation_x}deg) rotateY(${final_rotation_y}deg)`;
     });
 })();
