86a99ecb2b1773b43a0058787e425aa29da689c8
Simon Große Beckmann <simon.groseb@gmail.com>
2026-04-19 14:04:39 +0200

    Update die rotation logic to prevent duplicates
    Refactor die click event to avoid repeated rotations.

--- assets/js/pages/d6.js
+++ assets/js/pages/d6.js
@@ -1,5 +1,8 @@
 (() => {
     const die = document.querySelector('.die');
+    let previous_rotation_x = 0;
+    let previous_rotation_y = 0;
+
     die.addEventListener("click", (event) => {
         const number = Math.floor(Math.random() * 6) + 1;
         let end_position_x, end_position_y;
@@ -31,15 +34,24 @@
             default:
                 break;
         }
-        const extra_rotation_x = (Math.random() * 3600);
-        const extra_rotation_y = (Math.random() * 3600);
-        const rotation_overflow_x = extra_rotation_x % 360;
-        const rotation_overflow_y = extra_rotation_y % 360;
-        const trimmed_rotations_x = extra_rotation_x - rotation_overflow_x;
-        const trimmed_rotations_y = extra_rotation_y - rotation_overflow_y;
-        const final_rotation_x = end_position_x + trimmed_rotations_x;
-        const final_rotation_y = end_position_y + trimmed_rotations_y;
+        
+        do {
+            const extra_rotation_x = (Math.random() * 3600);
+            const rotation_overflow_x = extra_rotation_x % 360;
+            const trimmed_rotations_x = extra_rotation_x - rotation_overflow_x;
+            const final_rotation_x = end_position_x + trimmed_rotations_x;
+        } while (final_rotation_x == previous_rotation_x);
+        
+        do {
+            const extra_rotation_y = (Math.random() * 3600);
+            const rotation_overflow_y = extra_rotation_y % 360;
+            const trimmed_rotations_y = extra_rotation_y - rotation_overflow_y;
+            const final_rotation_y = end_position_y + trimmed_rotations_y;
+        } while (final_rotation_y == previous_rotation_y);
 
+        previous_rotation_x = final_rotation_x;
+        previous_rotation_y = final_rotation_y;
+    
         // console.log(`number: ${number}`);
         // console.log(`end: ${end_position_x}, ${end_position_y}`);
         // console.log(`extra: ${extra_rotation_x}, ${extra_rotation_y}`);
